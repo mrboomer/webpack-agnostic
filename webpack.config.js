985847c052fdadceda453cd98442a4255816fc4d
Daniel Escobedo <daniel@escobedo.io>
2018-09-15 11:29:48 -0700

    Added Basic Setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,12 +14,47 @@ module.exports = {
         test: /\.html$/,
         use: [{ loader: 'html-loader', options: { minimize: true } }],
       },
+      {
+        test: /\.(png|jpe?g)/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: './img/[name].[ext]',
+              limit: 10000,
+            },
+          },
+          {
+            loader: 'img-loader',
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: 'src/index.html',
       filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
   ],
 };
